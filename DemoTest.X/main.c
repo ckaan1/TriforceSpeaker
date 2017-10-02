12dbb225a7252957936b61fca54188736eedb31d
Christian <clkaan@wpi.edu>
2017-10-02 15:20:20 -0600

    Added Interrupt timing test using led blink

--- DemoTest.X/main.c
+++ DemoTest.X/main.c
@@ -1,9 +1,28 @@
 #include <xc.h>
 
 
+/* Example code for Timer1 ISR*/
+void __attribute__((__interrupt__, __shadow__)) _T1Interrupt(void)
+{
+    /* Interrupt Service Routine code goes here */
+    IFS0bits.T1IF = 0; //Reset Timer1 interrupt flag and Return from ISR
+    LATAbits.LATA0 ^= 1;
+}
+
+
 int main(void)
 {
     unsigned int i;
+    
+    T1CON = 0x00; //Stops the Timer1 and reset control reg.
+    TMR1 = 0x00; //Clear contents of the timer register
+    PR1 = 0x7A12; //Load the Period register with the value 0xFFFF
+    IPC0bits.T1IP = 0x01; //Setup Timer1 interrupt for desired priority level
+
+    IFS0bits.T1IF = 0; //Clear the Timer1 interrupt status flag
+    IEC0bits.T1IE = 1; //Enable Timer1 interrupts
+    T1CONbits.TON = 3; //Start Timer1 with prescaler settings at 1:1 and
+    //clock source set to the internal instruction cycle
     
     // Set up DAC1CON
     // Port setup for DACs
@@ -40,7 +59,6 @@ int main(void)
         
 
         // Toggle the LED output pin to alternate between the LED being on and off
-        LATAbits.LATA0 ^= 1;
         DAC1DAT -= 0x8;
     }
 }
